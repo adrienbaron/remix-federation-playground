cad7ec3da0846c5b11ae281743b1f7424ac9650a
adrienbaron <adrien@abaron.net>
2022-02-15 22:38:15 +0000

    add action to remote page

--- remix-remote/app/routes/remote.tsx
+++ remix-remote/app/routes/remote.tsx
@@ -1,13 +1,24 @@
-import {Link, useLoaderData} from "@remix-run/react"
+import {Form, Link, useActionData, useLoaderData, useTransition} from "@remix-run/react"
 import {useState} from "react"
+import {ActionFunction} from "remix"
 
 export const loader = () => {
   return {"data": "This is some data from the loader"}
 }
 
+export const action: ActionFunction = async ({request}) => {
+  const body = await request.formData();
+
+  console.log("Got username from action:", body.get("username"))
+
+  return {"username": body.get("username")}
+}
+
 export default function Remote() {
   const [counter, setCounter] = useState(0)
   const loaderData = useLoaderData()
+  const transition = useTransition()
+  const actionData = useActionData()
 
   return (
       <div style={{fontFamily: "system-ui, sans-serif", lineHeight: "1.4"}}>
@@ -19,6 +30,11 @@ export default function Remote() {
           </div>
           <button onClick={() => setCounter(c => c + 1)}>Increase Counter</button>
         </div>
+        <Form method="post">
+          <input type="text" name="username" defaultValue="someName"/>
+          <button disabled={transition.state === "loading"}>Submit</button>
+          {actionData && <p>You submitted to the action: {actionData.username}</p>}
+        </Form>
         <Link to="/">Back to host</Link>
       </div>
   )
